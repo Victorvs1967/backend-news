f988a764dbfdcb6b7043ac26e13a81c6f9944aa2
Victors1967 <victorsmirnov@me.com>
2021-04-05 00:06:38 +0300

    cross origon

--- src/main/java/com/vvs/springnewsparser/api/NewsController.java
+++ src/main/java/com/vvs/springnewsparser/api/NewsController.java
@@ -6,10 +6,12 @@ import com.vvs.springnewsparser.model.News;
 import com.vvs.springnewsparser.service.NewsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class NewsController {
 
     @Autowired
