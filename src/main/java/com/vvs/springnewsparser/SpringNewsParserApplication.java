dc9bf3f96efd9fb947c62d4093ed67a3e14987c8
Victors1967 <victorsmirnov@me.com>
2021-04-30 00:57:17 +0300

    111

--- src/main/java/com/vvs/springnewsparser/SpringNewsParserApplication.java
+++ src/main/java/com/vvs/springnewsparser/SpringNewsParserApplication.java
@@ -1,9 +1,11 @@
 package com.vvs.springnewsparser;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@SpringBootConfiguration
 @SpringBootApplication
 @EnableScheduling
 public class SpringNewsParserApplication {
